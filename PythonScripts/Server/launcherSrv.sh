44fb4c41c088e83654596075f4ab28901812ddfc
BigZee <git@myownworld.eu>
2021-04-07 21:44:33 +0200

    Added launcher + fixes

---
+++ PythonScripts/Server/launcherSrv.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# launcherSrv.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+
+
+cd /
+cd home/pi/Pythons
+sudo python3 server.py
+cd /
--- PythonScripts/Server/server.py
+++ PythonScripts/Server/server.py
@@ -1,3 +1,4 @@
+# !/bin/python
 import RPi.GPIO as GPIO
 import os
 from time import sleep
